140ebd40675b890715630ec92060299699b183af
Touta <abnatta.oumar-berdiji@univ-tlse3.fr>
2025-11-07 16:55:04 +0100

    Update Gaulois.java

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,6 +4,7 @@ public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	private Village village;
 
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -23,14 +24,14 @@ public class Gaulois {
 	}
 
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		System.out.println(nom + " envoie un grand coup dans la mÃ¢choire de " + romain.getNom());
 		romain.recevoirCoup((force * effetPotion) / 3);
 		if (effetPotion > 1) effetPotion--;
 	}
 
 	public void boirePotion(int forcePotion) {
 		effetPotion = forcePotion;
-		System.out.println("« Merci Druide, je sens que ma force est " + effetPotion + " fois décuplée ! »");
+		System.out.println("Â« Merci Druide, je sens que ma force est " + effetPotion + " fois dÃ©cuplÃ©e ! Â»");
 	}
 
 	public String toString() {
@@ -38,7 +39,28 @@ public class Gaulois {
 	}
 
 	public static void main(String[] args) {
-		Gaulois asterix = new Gaulois("Astérix", 8);
+		Gaulois asterix = new Gaulois("AstÃ©rix", 8);
 		System.out.println(asterix);
 	}
+	   public void setVillage(Village village) {
+        this.village = village;
+    }
+
+    public Village getVillage() {
+        return village;
+    }
+
+    public void sePresenter() {
+        if (village == null) {
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom
+                    + ". Je voyage de villages en villages.\"");
+        } else if (village.getChef() != null && village.getChef().equals(this)) {
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom
+                    + ". Je suis le chef du village " + village.getNom() + ".\"");
+        } else {
+            System.out.println("Le Gaulois " + nom + " : \"Bonjour, je m'appelle " + nom
+                    + ". J'habite le village " + village.getNom() + ".\"");
+        }
+    }
 }
+
